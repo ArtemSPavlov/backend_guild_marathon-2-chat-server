62a243fc0a2f257c71f94bf5a647d1fb699c9b9b
ArtemPavlov <pavlov329811@gmail.com>
2022-01-29 19:33:53 +0300

    add connection to chat room

--- index.ts
+++ index.ts
@@ -1,15 +1,36 @@
 import * as net from "net";
+import { Socket } from "net";
 
 import { port } from "./config";
 
-const server = net.createServer((socket) => {
-  socket.write('hello!');
-  socket.end('goodbye\n');
-}).on('error', (err) => {
-  console.log(err);
-  throw err;
-});
+const server = net
+  .createServer(connectionListener)
+  .listen(port, () => {
+    console.log("opened server on", server.address());
+  })
+  .on("error", (err) => {
+    console.log(err);
+    throw err;
+  });
 
-server.listen(port, () => {
-  console.log('opened server on', server.address());
-});
+function connectionListener(socket: Socket): void {
+  socket.write("Enter chat room name >");
+
+  let roomName: string;
+  let nickName: string;
+
+  socket.on("data", data => {
+    const message: string = data.toString("utf8");
+
+    if (!roomName){
+      roomName = message;
+      socket.write("Enter nickname >");
+    } else {
+      nickName = message;
+
+      console.log(`Connect to room "${roomName}" with nickname "${nickName}"`);
+
+      server.removeListener("connection", connectionListener);
+    }
+  })
+}
