0a193bacd6d1533d18d2a4daa15185987f737dec
ArtemPavlov <pavlov329811@gmail.com>
2022-01-30 23:54:33 +0300

    rename "connection" to "user"

--- ChatRoom.ts
+++ ChatRoom.ts
@@ -1,6 +1,6 @@
 import { Socket } from "net";
 
-type ConnectionMetaData = {
+type UserMetaData = {
   nickName: string
 }
 
@@ -12,7 +12,7 @@ export default class ChatRoom {
   private static readonly rooms: Map<string, ChatRoom> = new Map();
 
   private readonly name: string;
-  private readonly connections: Map<Socket, ConnectionMetaData> = new Map();
+  private readonly users: Map<Socket, UserMetaData> = new Map();
   private readonly messages: string[] = [];
   private readonly commands: Map<string, ChatCommand> = new Map([
     [
@@ -29,10 +29,10 @@ export default class ChatRoom {
     this.name = name;
   }
 
-  public static start(roomName: string, firstConnection: Socket, nickName: string): void{
+  public static start(roomName: string, firstUser: Socket, nickName: string): void{
     const room: ChatRoom = ChatRoom.rooms.get(roomName) || ChatRoom.open(roomName);
 
-    room.add(firstConnection, nickName);
+    room.addUser(firstUser, nickName);
   }
 
   private static open(name: string): ChatRoom{
@@ -42,51 +42,49 @@ export default class ChatRoom {
     return room;
   }
 
-  public add(connection: Socket, nickName: string): void{
-    this.connections.set(connection, { nickName });
-    this.setListeners(connection);
+  public addUser(user: Socket, nickName: string): void{
+    this.users.set(user, { nickName });
+    this.setListeners(user);
     this.sendMessage(`%%${this.name}%%`, `User "${nickName}" connected`);
   }
 
-  private setListeners(connection: Socket): void{
-    connection.on("data", data => this.dataListener(data, connection));
-    connection.on("end", () => this.endListener(connection));
-    connection.on("error", () => this.endListener(connection));
+  private setListeners(user: Socket): void{
+    user.on("data", data => this.dataListener(data, user));
+    user.on("end", () => this.endListener(user));
+    user.on("error", () => this.endListener(user));
   }
 
-  private dataListener(data: Buffer, connection: Socket): void{
+  private dataListener(data: Buffer, user: Socket): void{
     const message: string = data.toString("utf8");
     const command: ChatCommand = this.commands.get(message);
 
     if(command){
-      connection.write(command());
+      user.write(command());
     } else {
-      const { nickName } = this.connections.get(connection);
+      const { nickName } = this.users.get(user);
 
       this.sendMessage(nickName, data.toString("utf8"));
     }
   }
 
-  private endListener(connection: Socket){
-    const { nickName } = this.connections.get(connection);
+  private endListener(user: Socket){
+    const { nickName } = this.users.get(user);
 
-    connection.removeAllListeners();
+    user.removeAllListeners();
     this.sendMessage(`%%${this.name}%%`, `User "${nickName}" disconnected`);
-    this.connections.delete(connection);
+    this.users.delete(user);
 
-    if (!this.connections.size) ChatRoom.close(this.name);
+    if (!this.users.size) ChatRoom.close(this.name);
   }
 
   private static close(roomName: string): void{
     this.rooms.delete(roomName);
   }
 
-  private errorHandler(connections: Socket): void{}
-
   private sendMessage(author: string, text: string): string{
     const time = ChatRoom.getCurrentTime();
     const message = `[${time}] ${author}: ${text}`;
-    const sockets = Array.from(this.connections.keys());
+    const sockets = Array.from(this.users.keys());
 
     sockets.forEach(socket => socket.write(message));
 
@@ -109,7 +107,7 @@ export default class ChatRoom {
   private getNickNames(): string{
     const TITLE = "\n ----- USERS -----\n";
     const nickNames = Array
-      .from(this.connections.values())
+      .from(this.users.values())
       .map(({nickName}) => nickName);
 
     return `${TITLE}${nickNames.join("\n")}${TITLE}`;
